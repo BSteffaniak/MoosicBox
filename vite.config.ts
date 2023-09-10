db54ade0a79ece381a2e1a61ce0d19ca9afaab75
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-09-09 20:33:29 -0400

    Update formatting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import aws from "solid-start-sst";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ adapter: aws() })],
-  optimizeDeps: { exclude: ["sst"] },
+    plugins: [solid({ adapter: aws() })],
+    optimizeDeps: { exclude: ["sst"] },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,21 +2,21 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [solid()],
-  test: {
-    deps: {
-      registerNodeLoader: true,
-      inline: [/solid-js/],
+    plugins: [solid()],
+    test: {
+        deps: {
+            registerNodeLoader: true,
+            inline: [/solid-js/],
+        },
+        environment: "jsdom",
+        globals: true,
+        setupFiles: [
+            "node_modules/@testing-library/jest-dom/extend-expect",
+            "./setupVitest.js",
+        ],
+        transformMode: { web: [/\.[jt]sx?$/] },
     },
-    environment: "jsdom",
-    globals: true,
-    setupFiles: [
-      "node_modules/@testing-library/jest-dom/extend-expect",
-      "./setupVitest.js",
-    ],
-    transformMode: { web: [/\.[jt]sx?$/] },
-  },
-  resolve: {
-    conditions: ["development", "browser"],
-  },
+    resolve: {
+        conditions: ["development", "browser"],
+    },
 });
