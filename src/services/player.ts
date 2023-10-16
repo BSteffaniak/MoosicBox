2e72056ec2d46d3318479a4a5c57c4774c433956
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-10-16 07:30:00 -0400

    Add event listeners for all player actions

--- src/services/player.ts
+++ src/services/player.ts
@@ -9,6 +9,30 @@ export type TrackListenerCallback = (
     position: number,
 ) => void;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type BaseCallbackType = (...args: any) => any;
+function createListener<CallbackType extends BaseCallbackType>(): {
+    on: (callback: CallbackType) => CallbackType;
+    off: (callback: CallbackType) => void;
+    listeners: CallbackType[];
+    trigger: CallbackType;
+} {
+    let listeners: CallbackType[] = [];
+    function on(callback: CallbackType): CallbackType {
+        listeners.push(callback);
+        return callback;
+    }
+    function off(callback: CallbackType): void {
+        listeners = listeners.filter((c) => c !== callback);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const trigger = (...args: any) => {
+        listeners.forEach((callback) => callback(...args));
+    };
+
+    return { on, off, listeners, trigger: trigger as CallbackType };
+}
+
 export const [sound, setSound] = createSignal<Howl>();
 export const [playing, setPlaying] = createSignal(false, { equals: false });
 export const [currentSeek, setCurrentSeek] = makePersisted(
@@ -78,84 +102,109 @@ export interface PlayerType {
     nextTrack(): boolean;
 }
 
+const playListener = createListener<() => void>();
+export const onPlay = playListener.on;
+export const offPlay = playListener.off;
+
 export function play() {
     player.play();
+    playListener.trigger();
 }
+
+const seekListener = createListener<() => void>();
+export const onSeek = seekListener.on;
+export const offSeek = seekListener.off;
 
 export function seek(seek: number) {
     player.seek(seek);
+    seekListener.trigger();
 }
+
+const pauseListener = createListener<() => void>();
+export const onPause = pauseListener.on;
+export const offPause = pauseListener.off;
 
 export function pause() {
     player.pause();
+    pauseListener.trigger();
 }
 
-let previousTrackListeners: TrackListenerCallback[] = [];
-export function onPreviousTrack(
-    callback: TrackListenerCallback,
-): TrackListenerCallback {
-    previousTrackListeners.push(callback);
-    return callback;
-}
-export function offPreviousTrack(callback: TrackListenerCallback): void {
-    previousTrackListeners = previousTrackListeners.filter(
-        (c) => c !== callback,
-    );
-}
+const prevTrackListener = createListener<TrackListenerCallback>();
+export const onPreviousTrack = prevTrackListener.on;
+export const offPreviousTrack = prevTrackListener.off;
 
 export function previousTrack(): boolean {
     if (player.previousTrack()) {
-        previousTrackListeners.forEach((callback) =>
-            callback(currentTrack()!, playlistPosition()!),
-        );
+        prevTrackListener.trigger(currentTrack()!, playlistPosition()!);
         return true;
     }
     return false;
 }
 
-let nextTrackListeners: TrackListenerCallback[] = [];
-export function onNextTrack(
-    callback: TrackListenerCallback,
-): TrackListenerCallback {
-    nextTrackListeners.push(callback);
-    return callback;
-}
-export function offNextTrack(callback: TrackListenerCallback): void {
-    nextTrackListeners = nextTrackListeners.filter((c) => c !== callback);
-}
+const nextTrackListener = createListener<TrackListenerCallback>();
+export const onNextTrack = nextTrackListener.on;
+export const offNextTrack = nextTrackListener.off;
 
 export function nextTrack(): boolean {
     if (player.nextTrack()) {
-        nextTrackListeners.forEach((callback) =>
-            callback(currentTrack()!, playlistPosition()!),
-        );
+        nextTrackListener.trigger(currentTrack()!, playlistPosition()!);
         return true;
     }
     return false;
 }
 
+const stopListener = createListener<() => void>();
+export const onStop = stopListener.on;
+export const offStop = stopListener.off;
+
 export function stop() {
     player.stop();
+    stopListener.trigger();
 }
+
+const playAlbumListener = createListener<() => void>();
+export const onPlayAlbum = playAlbumListener.on;
+export const offPlayAlbum = playAlbumListener.off;
 
 export async function playAlbum(album: Api.Album | Api.Track) {
     await player.playAlbum(album);
+    playAlbumListener.trigger();
 }
+
+const playPlaylistListener = createListener<() => void>();
+export const onPlayPlaylist = playPlaylistListener.on;
+export const offPlayPlaylist = playPlaylistListener.off;
 
 export function playPlaylist(tracks: Api.Track[]) {
     player.playPlaylist(tracks);
+    playPlaylistListener.trigger();
 }
+
+const addAlbumToQueueListener = createListener<() => void>();
+export const onAddAlbumToQueue = addAlbumToQueueListener.on;
+export const offAddAlbumToQueue = addAlbumToQueueListener.off;
 
 export async function addAlbumToQueue(album: Api.Album | Api.Track) {
     player.addAlbumToQueue(album);
+    addAlbumToQueueListener.trigger();
 }
+
+const removeTrackFromPlaylistListener = createListener<() => void>();
+export const onRemoveTrackFromPlaylist = removeTrackFromPlaylistListener.on;
+export const offRemoveTrackFromPlaylist = removeTrackFromPlaylistListener.off;
 
 export function removeTrackFromPlaylist(index: number) {
     player.removeTrackFromPlaylist(index);
+    removeTrackFromPlaylistListener.trigger();
 }
+
+const playFromPlaylistPositionListener = createListener<() => void>();
+export const onPlayFromPlaylistPosition = playFromPlaylistPositionListener.on;
+export const offPlayFromPlaylistPosition = playFromPlaylistPositionListener.off;
 
 export function playFromPlaylistPosition(index: number) {
     player.playFromPlaylistPosition(index);
+    playFromPlaylistPositionListener.trigger();
 }
 
 export const player: PlayerType = {} as PlayerType;
