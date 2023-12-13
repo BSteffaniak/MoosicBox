be778eb01b9187551b0def65f47307d1b6b468c0
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-12-13 07:30:00 -0500

    Add clientId and token inputs to settings page

--- src/routes/(global)/settings/(settings).tsx
+++ src/routes/(global)/settings/(settings).tsx
@@ -1,10 +1,42 @@
+import { Api } from '~/services/api';
 import './settings.css';
 
 export default function settingsPage() {
+    let clientIdInput: HTMLInputElement;
+
+    function saveClientId() {
+        Api.setClientId(clientIdInput.value);
+    }
+
+    let tokenInput: HTMLInputElement;
+
+    function saveToken() {
+        Api.setToken(tokenInput.value);
+    }
+
     return (
         <div>
             <ul>
-                <li></li>
+                <li>
+                    Client ID:{' '}
+                    <input
+                        ref={clientIdInput!}
+                        type="text"
+                        value={Api.clientId()}
+                        onKeyUp={(e) => e.key === 'Enter' && saveClientId()}
+                    />
+                    <button onClick={saveClientId}>save</button>
+                </li>
+                <li>
+                    Token:{' '}
+                    <input
+                        ref={tokenInput!}
+                        type="text"
+                        value={Api.token()}
+                        onKeyUp={(e) => e.key === 'Enter' && saveToken()}
+                    />
+                    <button onClick={saveToken}>save</button>
+                </li>
             </ul>
         </div>
     );
