3640fd7314e6b553a61d714a67bb5d3434b919e4
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-08-14 07:30:00 -0400

    Add deepEqual function

--- src/services/util.ts
+++ src/services/util.ts
@@ -350,3 +350,25 @@ export const trace = console.trace;
 export function throwExpression(errorMessage: string): never {
     throw new Error(errorMessage);
 }
+
+export function deepEqual<T>(x: T, y: T) {
+    if (x === y) {
+        return true;
+    } else if (
+        typeof x == 'object' &&
+        x != null &&
+        typeof y == 'object' &&
+        y != null
+    ) {
+        if (Object.keys(x).length != Object.keys(y).length) return false;
+
+        for (const prop in x) {
+            // eslint-disable-next-line no-prototype-builtins
+            if (y.hasOwnProperty(prop)) {
+                if (!deepEqual(x[prop], y[prop])) return false;
+            } else return false;
+        }
+
+        return true;
+    } else return false;
+}
