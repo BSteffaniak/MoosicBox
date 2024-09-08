95a45265033b5fab2bd58a355c2ce36262524fc7
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-09-08 13:24:56 -0400

    #2 Add htmx

---
+++ src/middleware/htmx.ts
@@ -0,0 +1,37 @@
+import htmx from 'htmx.org';
+import type { HtmxRequestConfig } from 'htmx.org';
+import { isServer } from 'solid-js/web';
+import { getConnection } from '~/services/api';
+
+type MutableConfig = {
+    -readonly [K in keyof typeof htmx.config]: (typeof htmx.config)[K];
+};
+
+if (!isServer) {
+    const config: MutableConfig = htmx.config;
+    config.selfRequestsOnly = false;
+
+    document.body.addEventListener('htmx:configRequest', (event) => {
+        if (!('detail' in event))
+            throw new Error(`Invalid htmx event: ${JSON.stringify(event)}`);
+
+        const con = getConnection();
+        const detail = event.detail as HtmxRequestConfig;
+
+        const clientId = con.clientId;
+
+        if (clientId) {
+            detail.parameters.clientId = clientId;
+        }
+
+        const token = con.staticToken || con.token;
+
+        if (token && !detail.headers.Authorization) {
+            detail.headers.Authorization = token;
+        }
+
+        detail.path = `${con.apiUrl}${detail.path}`;
+    });
+}
+
+export { htmx };
